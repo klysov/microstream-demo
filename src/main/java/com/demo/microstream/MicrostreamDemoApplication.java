50bc39f6c56df8d05a8675bcac7e4e6b4ac91d14
Kirill Lysov <klysov@inspirebrands.com>
2022-02-25 11:34:12 +0300

    Initial commit

---
+++ src/main/java/com/demo/microstream/MicrostreamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.microstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostreamDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrostreamDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/microstream/MicrostreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.microstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrostreamDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
